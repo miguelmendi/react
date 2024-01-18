104687e60def20e34ad235d941bccd2b82a20cf6
Miguel <miguelmds925@gmail.com>
2024-01-18 18:10:20 -0300

    Vendo algumas propriedades especificas do JSX

--- ferramentas-frontend/src/App.jsx
+++ ferramentas-frontend/src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
+const titulo = <h1>Esse é um título</h1>;
+
 const App = () => {
-  return <div>App React</div>;
+  const nome = 'Miguel';
+  const ativo = true;
+
+  function mostrarNome(sobrenome) {
+    return 'Miguel' + sobrenome;
+  }
+
+  const carro = {
+    marca: 'Ford',
+    rodas: '4',
+  };
+
+  const estiloP = {
+    color: 'blue',
+    fontSize: '2rem',
+  };
+
+  return (
+    <>
+      {titulo}
+      <p>
+        {true ? 'asasa' : 'sdasdas'} - {10}
+        {mostrarNome('Mendes')}
+      </p>
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p style={estiloP}>{carro.marca}</p>
+      <p style={estiloP}>{carro.rodas}</p>
+      <p className={ativo ? 'ativo' : 'inativo'}>{nome}</p>
+    </>
+  );
 };
 
 export default App;
