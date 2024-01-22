5c2ec6dd4a780f06c819d5da3a11df39051df41a
Miguel <miguelmds925@gmail.com>
2024-01-22 10:27:46 -0300

    Resolvendo o exercício sobre arrays

--- ferramentas-frontend/src/App.jsx
+++ ferramentas-frontend/src/App.jsx
@@ -1,69 +1,47 @@
 import React from 'react';
 
-// Mostre os dados da aplicação, como aprensetado no vídeo
-// Não utilize CSS externo, use o style para mudar as cores
-// Se a situação estiver ativa pinte de verde, inativa vermelho
-// Se o gasto for maior que 10000 mostre uma mensagem
-/*const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-  ],
-  ativa: true,
-};
-
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-    { nome: 'Notebook', preco: 'R$ 2500' },
-    { nome: 'Geladeira', preco: 'R$ 3000' },
-    { nome: 'Smartphone', preco: 'R$ 1500' },
-    { nome: 'Guitarra', preco: 'R$ 3500' },
-  ],
-  ativa: false,
-};*/
+// Organize os produtos como mostrado no vídeo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
 const App = () => {
-  /*const dados = mario;
-
-  const total = dados.compras
-    .map((item) => Number(item.preco.replace('R$', '')))
-    .reduce((a, b) => a + b);
-
   return (
-    <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:{' '}
-        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total: R$ {total}</p>
-      {total > 10000 && <p>Você está gastando muito</p>}
-    </div> 
-  );*/
-
-  const livros = [
-    { nome: 'A Game of Thrones', ano: 1996 },
-    { nome: 'A Clash of Kings', ano: 1998 },
-    { nome: 'A Storm of Swords', ano: 2000 },
-  ];
-  return (
-    <ul>
-      {livros
-        .filter(({ ano }) => 1998)
-        .map(({ nome, ano }) => {
-          <li key={nome}>
-            {nome}, {ano}
-          </li>;
-        })}
-    </ul>
+    <section>
+      {produtos
+        .filter((produto) => Number(produto.preco.replace('R$ ', '')) > 1500)
+        .map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+            <p>Preço: {produto.preco}</p>
+            <ul>
+              {produto.cores.map((cor) => (
+                <li key={cor} style={{ backgroundColor: cor, color: 'white' }}>
+                  {cor}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+    </section>
   );
 };
 
